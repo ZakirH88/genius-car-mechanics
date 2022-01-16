dca7bfa47c1ee43748d93a7fe05ba4da8cac0fbe
ZakirH88 <cmzakirh77@gmail.com>
2022-01-16 07:44:29 +0600

    Router set commit

--- src/Pages/Home/Service/Service.js
+++ src/Pages/Home/Service/Service.js
@@ -1,17 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Service.css';
 
 const Service = ({service}) => {
 	// const {service} = props;
-	const {name, price, description, img} = service;
+	const {id, name, price, description, img} = service;
 
 	return (
-		<div className='service'>
+		<div className='service pb-3'>
 			
 			<img src = {img} alt='' />
 			<h2>{name}</h2>
 			<h4>Price : {price}</h4>
 			<p className='px-3'>{description}</p>
+			<Link to={`/booking/${id}`}>
+				<button className="btn btn-warning">Book {name.toLowerCase()} </button>
+			</Link>
 		</div>
 	);
 };
